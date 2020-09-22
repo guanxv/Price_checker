41c28b9423180d6fdcf67b651e375dbcd8e0aca1
Xu Guan <guanxv@gmail.com>
2020-09-23 00:04:01 +1000

    Scraper for Carsales added

--- scraperCarsales.py
+++ scraperCarsales.py
@@ -12,7 +12,7 @@ URL = "https://www.carsales.com.au/cars/used/volkswagen/golf/victoria-state/"
 
 def getMultiPageRespondCarsales(theURL, totalPages):
 
-    print('Start Sending Requests...')
+    print("Start Sending Requests...")
 
     allPages = []
 
@@ -26,7 +26,7 @@ def getMultiPageRespondCarsales(theURL, totalPages):
 
         # generate request
         payload = {"offset": (pageNum - 1) * 12}
-        #payload = {"offset": 548}
+        # payload = {"offset": 548}
         headers = {
             "User-Agent": "Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/78.0.3904.97 Safari/537.36"
         }
@@ -37,7 +37,7 @@ def getMultiPageRespondCarsales(theURL, totalPages):
 
         allPages.append(the_page)
 
-        print("page", pageNum, "respond received. Total Page Number ", totalPages )
+        print("page", pageNum, "respond received. Total Page Number ", totalPages)
 
     return allPages
 
@@ -56,33 +56,48 @@ def respondProcessCarsales(the_pages):
 
         for car_list in data_cars_json["mainEntity"]["itemListElement"]:
 
-            car_list = car_list["item"] #remove not wanted keys
+            car_list = car_list["item"]  # remove not wanted keys
 
-            car_list["year"] = int(car_list["name"].split(" ")[0]) # add year in int type
+            car_list["year"] = int(
+                car_list["name"].split(" ")[0]
+            )  # add year in int type
 
-            car_list['engineSize'] = float(car_list['vehicleEngine']['engineDisplacement']['value'].split(" ")[1].strip("L")) # add engine size in liter in int 
+            car_list["engineSize"] = float(
+                car_list["vehicleEngine"]["engineDisplacement"]["value"]
+                .split(" ")[1]
+                .strip("L")
+            )  # add engine size in liter in int
 
-            car_list['numberOfCylinder'] = int(car_list['vehicleEngine']['engineDisplacement']['value'].split(" ")[0].strip("cyl")) # add number of cylinder in num
+            car_list["numberOfCylinder"] = int(
+                car_list["vehicleEngine"]["engineDisplacement"]["value"]
+                .split(" ")[0]
+                .strip("cyl")
+            )  # add number of cylinder in num
 
-            if "Petrol".lower() in car_list['vehicleEngine']['engineDisplacement']['value']:  # add fuel type
-                
-                car_list['fuelType'] = "Petrol"
+            if (
+                "Petrol".lower()
+                in car_list["vehicleEngine"]["engineDisplacement"]["value"]
+            ):  # add fuel type
+
+                car_list["fuelType"] = "Petrol"
 
             else:
 
-                car_list['fuelType'] = "Diesel"
+                car_list["fuelType"] = "Diesel"
 
-            
-            car_list['odometer'] = int(car_list['mileageFromOdometer']['value']) # move odometer key in top level for easier access, change to int type
+            car_list["odometer"] = int(
+                car_list["mileageFromOdometer"]["value"]
+            )  # move odometer key in top level for easier access, change to int type
 
-            if "private" in car_list['image'][0]['url']:   # is it private or dealer car , add list type in 'string'
+            if (
+                "private" in car_list["image"][0]["url"]
+            ):  # is it private or dealer car , add list type in 'string'
 
-                car_list['list_type'] = "private"
+                car_list["list_type"] = "private"
 
             else:
 
-                car_list['list_type'] = "dealer"
-
+                car_list["list_type"] = "dealer"
 
             all_json.append(car_list)  # append to json,
 
